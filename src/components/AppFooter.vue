1c7d1f547c2620f6dbe183096c7abb1824ea60c6
Habra26 <arnaud.habraken@outlook.com>
2025-11-17 15:10:01 +0100

    Architecture des components

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <footer class="my-6 text-slate-400 text-sm text-center">
+      <p>Simple Todo Template — Tailwind CSS · EAFC 2025</p>
+    </footer>
+</template>
+
+<style scoped></style>
---
+++ src/components/todoList/TodoList.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Todo from './Todo.vue';
+import TodoListAddForm from './TodoListAddForm.vue';
+import TodolistFooter from './TodolistFooter.vue';
+
+
+</script>
+
+<template>
+  <section
+    class="bg-slate-100 rounded-xl shadow ring-1 ring-slate-200/60 overflow-hidden"
+    aria-labelledby="todo-heading"
+  >
+    <h2 id="todo-heading" class="sr-only">Todo list</h2>
+
+    <!-- INPUT PRINCIPAL -->
+    <TodoListAddForm />
+
+    <!-- LISTE DES TODOS -->
+    <ul class="m-4 divide-y divide-slate-200" role="list" aria-label="Todos">
+      <!-- ITEM (exemple) -->
+      <Todo />
+    </ul>
+
+    <!-- FOOTER DE LISTE -->
+    <TodolistFooter />
+  </section>
+</template>
+
+<style scoped></style>
